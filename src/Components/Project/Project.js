743356cc3b1d4399afa6e8b0357e84804401935f
Aldrich Reboja <aktreboja@gmail.com>
2021-05-05 10:52:58 -0700

    F

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -8,6 +8,19 @@ const Project = (props) => {
     return (
         <div className = "Project">
             <img src = {seven} />
+            <div className = "Project-Description">
+                    <p className = "Project-Title">7yearslater</p>
+
+                    
+                    <p className = "Project-Text">A wedding based Web Application that consists of a backend, allowing users to RSVP an lqwksdjvchqsldkjchb</p>
+                    <div className = "Stack-Container">
+                        <p className = "Stack">ReactJS</p>
+                        <p className = "Stack">Node.JS</p>
+                        <p className = "Stack">Express</p>
+                        <p className = "Stack">MySQL</p>
+                    </div>
+                    
+                </div>
      </div>
     )
 }
--- src/Containers/ProjectContainer/ProjectContainer.js
+++ src/Containers/ProjectContainer/ProjectContainer.js
@@ -5,15 +5,12 @@ import ProjectModal from '../../Components/ProjectModal/ProjectModal'
 
 import seven from '../../Images/7yearslater.jpeg'
 
+
 export default class ProjectContainer extends Component {
     render() {
         return (
             <div className = "Project-Container">
                 <Project projectImg = {seven} />
-                <div className = "Project-Description">
-                    <h1>7yearslater</h1>
-                    <p>A web app that consists of qsldwkvjbqkljdsvbljkhb</p>
-                </div>
             </div>
         )
     }
